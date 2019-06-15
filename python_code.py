45ab19a161c59d3219813c0d5c04b0b59ba33d7e
unknown <you@example.com>
2019-06-15 22:17:03 +0200

    for final assignment

--- python_code.py
+++ python_code.py
@@ -1,8 +1,6 @@
-# this notebook is for the final assignment 
-# it's a first time i use github:)
-
-import pandas as pd
-import numpy as np
-
-print('Hello Capstone project Course!')
+# this notebook is use for final assignment
+# it's my first time on github
+ import pandas as pd
+ import numpy as np
+ print("Hello Capstone Project Course!")
 
